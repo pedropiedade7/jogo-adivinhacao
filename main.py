f52380b068c04d48f70ba31d0241b6461c5844e7
pedropiedade7 <pedropiedade864@gmail.com>
2024-03-11 09:59:58 -0300

    feat: option try choices

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import random
 
 def jogo_adivinhacao():
     numero_secreto = random.randint(1, 100)
-    tentativas = 5
-
+    tentativas = 0
+    escolha = 0
     print("Bem-vindo ao jogo de adivinhação!")
     print("Tente adivinhar o número secreto entre 1 e 100.")
+
+    while int(escolha) != 1 and int(escolha) != 2 and int(escolha) != 3:
+        escolha = input("Selecione a dificuldade \n 1 - Facil \n 2 - Médio \n 3 - Dificil \n")
+        if int(escolha) == 3:
+            tentativas = 2
+        elif int(escolha) == 2:
+            tentativas = 4
+        elif int(escolha) == 1:
+            tentativas = 8
+        else: 
+            print("Selecione um número de tentativas válido! ")
+
     print(f"Você tem {tentativas} tentativas.")
 
     while tentativas > 0:
